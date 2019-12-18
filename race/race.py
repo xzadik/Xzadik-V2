ded5f6e360bb40ea0f61a120a1c716e765869316
Xzadik| <53629779+XzadikApple@users.noreply.github.com>
2019-12-18 03:45:40 -0500

    Update race.py

--- race/race.py
+++ race/race.py
@@ -17,17 +17,16 @@ from .utils.dataIO import dataIO
 creditIcon = "https://i.imgur.com/a0lbPXh.png"
 credits = "A Bot by: >_Xzadik & CRZA5"
 
-animals = ((':Weary:', 'fast'), (':Weary:', 'fast'), (':Weary:', 'fast'), (':Weary:', 'slow'),
-           (':Weary:', 'fast'), (':Weary:', 'fast'), (':Weary:', 'fast'), (':Weary:', 'fast'),
-           (':Weary:', 'steady'), (':Weary:', 'steady'), (':Weary:', 'steady'),
-           (':Weary:', 'steady'), (':Weary:', 'steady'), (':Weary:', 'abberant'),
-           (':Weary:', 'abberant'), (':Weary:', 'abberant'), (':Weary:', 'abberant'),
-           (':Weary:', 'abberant'), (':Weary:', 'abberant'), (':Weary:', 'abberant'),
-           (':Weary:', 'predator'), (':Weary:', 'predator'), (':Weary:', 'special'),
-           (':Weary:', 'special'), (':Weary:', 'slow'), (':Weary:', 'slow'), (':Weary:', 'slow'),
-           (':Weary:', 'slow'), (':Weary:', 'slow'), (':Weary:', 'slow'), (':Weary:', 'slow'),
-           (':Weary:', 'slow'), (':Weary:', 'slow'), (':Weary:', 'slow'))
-
+animals = ((':rabbit2:', 'fast'), (':monkey:', 'fast'), (':cat2:', 'fast'), (':mouse2:', 'slow'),
+           (':chipmunk:', 'fast'), (':rat:', 'fast'), (':dove:', 'fast'), (':bird:', 'fast'),
+           (':dromedary_camel:', 'steady'), (':camel:', 'steady'), (':dog2:', 'steady'),
+           (':poodle:', 'steady'), (':racehorse:', 'steady'), (':ox:', 'abberant'),
+           (':cow2:', 'abberant'), (':elephant:', 'abberant'), (':water_buffalo:', 'abberant'),
+           (':ram:', 'abberant'), (':goat:', 'abberant'), (':sheep:', 'abberant'),
+           (':leopard:', 'predator'), (':tiger2:', 'predator'), (':dragon:', 'special'),
+           (':unicorn:', 'special'), (':Weary:', 'slow'), (':bug:', 'slow'), (':rooster:', 'slow'),
+           (':snail:', 'slow'), (':scorpion:', 'slow'), (':crocodile:', 'slow'), (':pig2:', 'slow'),
+           (':turkey:', 'slow'), (':duck:', 'slow'), (':baby_chick:', 'slow'))
 
 
 class PluralDict(dict)
