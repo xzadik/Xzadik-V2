d119addfd7e04956ad2bae7fd395b542a1c33355
Xzadik| <53629779+XzadikApple@users.noreply.github.com>
2019-12-23 01:31:55 -0500

    Update rainbow.py

--- rainbow/rainbow.py
+++ rainbow/rainbow.py
@@ -24,8 +24,8 @@ class rainbow:
             no = discord.Embed(title="{} is not a valid role".format(role))
             await self.bot.say(embed=no)
             return
-        if interval < 3:
-            interval = 3
+        if interval < 120:
+            interval = 120
         while True:
             colour = ''.join([choice('0123456789ABCDEF') for x in range(6)])
             colour = int(colour, 16)
